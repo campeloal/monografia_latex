e7f71463128173f10e33c61ba4bc80464f184abb
campeloal <chevalier.beaumont@gmail.com>
2014-06-16 00:50:52 -0300

    final version

--- figuras/compl.py
+++ figuras/compl.py
@@ -1,31 +1,31 @@
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-xVertAxis = [1,2,3,4,5,6,7,8,9,10]
-constant = [20,20,20,20,20,20,20,20,20,20]
-n = [1,2,3,4,5,6,7,8,9,10]
-quad = [1,4,9,16,25,36,49,64,81,100]
-cubic = [1,8,27,64,125,216,343,512,729,1000]
-log = [np.log(1),np.log(2),np.log(3),np.log(4),np.log(5),np.log(6),
-       np.log(7),np.log(8),np.log(9),np.log(10)]
-nlog = [np.log(1),2 * np.log(2),3 * np.log(3),4 * np.log(4),5 * np.log(5),6 * np.log(6),
-       7 * np.log(7), 8 * np.log(8), 9 * np.log(9),10 * np.log(10)]
+xVertAxis = [1.0,1.5,2.0,2.5,3.0,3.5,4.0]
+constant = [10,10,10,10,10,10,10]
+n = xVertAxis
+quad = [np.power(xVertAxis[0],2),np.power(xVertAxis[1],2),np.power(xVertAxis[2],2),np.power(xVertAxis[3],2),np.power(xVertAxis[4],2),np.power(xVertAxis[5],2),
+        np.power(xVertAxis[6],2)]
+cubic = [np.power(xVertAxis[0],3),np.power(xVertAxis[1],3),np.power(xVertAxis[2],3),np.power(xVertAxis[3],3),np.power(xVertAxis[4],3),np.power(xVertAxis[5],3),
+         np.power(xVertAxis[6],3)]
+log = [np.log(xVertAxis[0]),np.log(xVertAxis[1]),np.log(xVertAxis[2]),np.log(xVertAxis[3]),np.log(xVertAxis[4]),np.log(xVertAxis[5]),np.log(xVertAxis[6])]
+nlog = [xVertAxis[0] * np.log(xVertAxis[0]),xVertAxis[1] * np.log(xVertAxis[1]),xVertAxis[2] * np.log(xVertAxis[2]),xVertAxis[3] * np.log(xVertAxis[3]),
+        xVertAxis[4] * np.log(xVertAxis[4]),xVertAxis[5] * np.log(xVertAxis[5]), xVertAxis[6] * np.log(xVertAxis[6])]
+exp = [np.exp(xVertAxis[0]),np.exp(xVertAxis[1]),np.exp(xVertAxis[2]),np.exp(xVertAxis[3]),np.exp(xVertAxis[4]),np.exp(xVertAxis[5]),np.exp(xVertAxis[6])]
 
-exp = [np.exp(1),np.exp(2),np.exp(3),np.exp(4),np.exp(5),np.exp(6),
-       np.exp(7),np.exp(8),np.exp(9),np.exp(10)]
-
-plt.title('Algorithm Complexity')
-plt.xlabel('Number of Inputs')
-plt.ylabel('Resulting size')
-plt.plot(xVertAxis, constant, 'y-',label='Constant')
+plt.title(u'Complexidade Assintótica')
+plt.xlabel(u'Número de entradas')
+plt.ylabel('Tamanho resultante')
+plt.plot(xVertAxis, constant, 'y-',label='Constante')
 plt.plot(xVertAxis, n, 'b-', label = "Linear")
-plt.plot(xVertAxis, quad, 'r-', label = "Quadric")
-plt.plot(xVertAxis, cubic, 'g-', label = "Cubic")
+plt.plot(xVertAxis, quad, 'r-', label = u"Quadrática")
+plt.plot(xVertAxis, cubic, 'g-', label = u"Cúbica")
 plt.plot(xVertAxis, log, 'm-', label = "Log N")
 plt.plot(xVertAxis, nlog, 'c-', label = "N Log N")
-plt.plot(xVertAxis, nlog, 'k-', label = "Exponential")
-plt.legend(loc="upper left", bbox_to_anchor=[0.7, 1.0],
-           ncol=1, shadow=True, title="Legend")
+plt.plot(xVertAxis, nlog, 'k-', label = "Exponencial")
+plt.legend(loc="upper left", bbox_to_anchor=[0.0, 1.0],
+           ncol=1, shadow=True, title="Legenda")
 
 plt.show()
